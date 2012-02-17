ff8ed42adc1dc25a2578a02b64888a89dd49da5e
boertel <ben@punchtab.com>
2012-02-16 23:03:25 -0800

    Slider is an object

--- slider.js
+++ slider.js
@@ -1,164 +1,153 @@
-var slider = {
-    positions: [],
-    width: 0,
-    current: 0,
-    length: 0,
-    elements: {
+var Slider = function (args) {
+    this.positions = [];
+    this.width = 0;
+    this.current = args.current || 0;
+    this.length = 0;
+    this.elements = {
         container: $('.slides'),
         pagination: $('.pagination'),
         next: $('.next'),
         previous: $('.previous')
-    },
-    active: 'active',
-    duration: 450,
-    timeout_duration: 1000,
-    callback: {},
-    auto: false,
-    key: {
+    };
+    this.active = args.active || 'active';
+    this.duration = args.duration || 450;
+    this.timeoutDuration = args.timeoutDuration || 1000;
+    this.callback = args.callback || {};
+    this.auto = args.auto || false;
+    this.key = args.key || {
         enable: true,
         previous: [37],
         next: [39]
-    },
-    hide: true,
+    };
+    this.hide = args.hide || true;
 
-    init: function (container) {
-        if (typeof container !== undefined) {
-            slider.elements.container = $('.slides', container);
-            slider.elements.next = $('.next', container);
-            slider.elements.previous = $('.previous', container);
-        }
-        slider.width = 0;
-        slider.current = 0;
-        slider.length = 0;
-        slider.positions = [];
+    var that = this;
 
-        slider.elements.container.find('.slide').each(function (i) {
-            slider.positions[i] = slider.width;
-            slider.width += $(this).outerWidth();
-        });
-        if (slider.elements.pagination.children().length !== 0) {
-            slider.length = slider.elements.pagination.children().length;
-        }
-        else {
-            slider.length = slider.positions.length;
-        }
-        slider.elements.container.find('.slide').show();
-        // Best to define the width of the container in CSS: it takes time to do it in js.
-        slider.elements.container.width(slider.width);
+    if (args.container !== undefined) {
+        this.elements.container = $('.slides', args.container);
+        this.elements.next = $('.next', args.container);
+        this.elements.previous = $('.previous', args.container);
+        this.elements.pagination = args.pagination || $(".pagination");
+    }
 
-        slider.place(slider.current);
-        slider.animate();
-        // Previous / Next buttons
-        slider.elements.next.click(function () {
-            return slider.bind.next();
-        });
-        slider.elements.previous.click(function () {
-            return slider.bind.previous();
-        })
+    this.elements.container.find('.slide').each(function (i) {
+        that.positions[i] = that.width;
+        that.width += $(this).outerWidth();
+    });
 
-        if (slider.key.enable) {
-            $(document).unbind().bind('keydown', slider.bind.keys);
-            $('input, textarea').focus(function() { $(document).unbind('keydown'); });
-            $('input, textarea').blur(function() { $(document).unbind().bind('keydown', slider.bind.keys); });
-        }
+    if (this.elements.pagination.children().length !== 0) {
+        this.length = this.elements.pagination.children().length;
+    } else {
+        this.length = this.positions.length;
+    }
+    this.elements.container.find(".slide").show();
+    // Best to define the width of the container in CSS: it takes time to do it in js.
+    this.elements.container.width(this.width);
 
-        // Pagination
-        slider.elements.pagination.children().each(function (i) {
-            $(this).click(function () {
-                slider.current = slider.place(i);
-                slider.animate(); 
-                return false;
-            });
-        });
-        
-        // Auto advance
-        slider.loop();
-    },
-    bind: {
-        move: function (name) {
-            var cb = true;
-            clearInterval(slider.timeout);
-            slider.loop();
-            slider.callback[name] && (cb = slider.callback[name]());
-            if (cb !== false) {
-                slider[name]();
-            }
+    this.place(this.current);
+    this.animate();
+    // Previous / Next buttons
+    this.elements.next.click(function () {
+        return that.move("next");
+    });
+    this.elements.previous.click(function () {
+        return that.move("previous");
+    });
+
+    if (this.key.enable) {
+        $(document).unbind().bind("keydown", $.proxy(this, "keys"));
+        $("input, textarea").focus(function() { $(document).unbind("keydown"); });
+        $("input, textarea").blur(function() { $(document).unbind().bind("keydown", $.proxy(this, "keys")); });
+    }
+
+    // Pagination
+    this.elements.pagination.children().each(function (i) {
+        $(this).click(function () {
+            that.current = that.place(i);
+            that.animate(); 
             return false;
-        },
-        next: function () {
-            return slider.bind.move('next');
-        },
-        previous: function () {
-            return slider.bind.move('previous');
-        },
-        keys: function (e) {
-            if (slider.key.previous.indexOf(e.keyCode) !== -1) {
-                slider.bind.previous();
-            }
-            if (slider.key.next && slider.key.next.indexOf(e.keyCode) !== -1) {
-                slider.bind.next();
-            }
-        }
-    },
-    place: function (i) {
-        if (i > slider.length-1) {
-            current = slider.length-1;
-        } else if (i < 0) {
-            current = 0;
-        }
-        else {
-            current = i;
-            if (slider.hide) {
-                slider.elements.next.show();
-                slider.elements.previous.show();
-            }
-        }
-
-        if (i >= slider.length-1 && slider.hide) {
-            slider.elements.next.hide();
-        }
-        if (i <= 0 && slider.hide) {
-            slider.elements.previous.hide();
-        }
-        if (slider.elements.pagination.length !== 0) {
-            slider.elements.pagination.children().removeClass(slider.active);
-            $(slider.elements.pagination.children()[current]).addClass(slider.active);
-        }
-        return current;
-    },
-    loop: function () {
-        if (slider.auto) {
-            slider.timeout = setTimeout(function () {
-                slider.current = (slider.current == slider.length-1) ? -1: slider.current;
-                slider.next();
-                slider.loop();
-            }, slider.timeout_duration);
-        }
-    },
-    next: function () {
-        var old = slider.current;
-        slider.current = slider.place(old+1);
-        if (slider.current !== old) {
-            slider.animate();
-        } 
-    },
-    previous: function () {
-        var old = slider.current;
-        slider.current = slider.place(old-1);
-        if (slider.current !== old) {
-            slider.animate();
-        } else {
-            slider.elements.previous.hide();
-        }
-    },
-    animate: function (animate) {
-        var args = {marginLeft: -slider.positions[slider.current]};
-        if (typeof animate !== undefined) {
-            slider.elements.container.stop().animate(args, slider.duration);
-        }
-        else {
-            slider.elements.container.css(args);
-        }
+        });
+    });
+    
+    // Auto advance
+    this.loop();
+};
+Slider.prototype.move = function (name) {
+    var cb = true;
+    clearInterval(this.timeout);
+    this.loop();
+    if (this.callback[name] !== undefined) {
+        cb = this.callback[name]();
+    }
+    if (cb !== false) {
+        this[name]();
+    }
+    return false;
+};
+Slider.prototype.keys = function (e) {
+    if (this.key.previous.indexOf(e.keyCode) !== -1) {
+        this.move('previous');
+    }
+    if (this.key.next && this.key.next.indexOf(e.keyCode) !== -1) {
+        this.move('next');
     }
 };
+Slider.prototype.place = function (i) {
+    if (i > this.length - 1) {
+        current = this.length - 1;
+    } else if (i < 0) {
+        current = 0;
+    } else {
+        current = i;
+        if (this.hide) {
+            this.elements.next.show();
+            this.elements.previous.show();
+        }
+    }
 
+    if (i >= this.length - 1 && this.hide) {
+        this.elements.next.hide();
+    }
+    if (i <= 0 && this.hide) {
+        this.elements.previous.hide();
+    }
+    if (this.elements.pagination.length !== 0) {
+        this.elements.pagination.children().removeClass(this.active);
+        $(this.elements.pagination.children()[current]).addClass(this.active);
+    }
+    return current;
+};
+Slider.prototype.loop = function () {
+    if (this.auto) {
+        var that = this;
+        this.timeout = setTimeout(function () {
+            that.current = (that.current == that.length-1) ? -1: that.current;
+            that.next();
+            that.loop();
+        }, this.timeout_duration);
+    }
+};
+Slider.prototype.next = function () {
+    var old = this.current;
+    this.current = this.place(old + 1);
+    if (this.current !== old) {
+        this.animate();
+    } 
+};
+Slider.prototype.previous = function () {
+    var old = this.current;
+    this.current = this.place(old-1);
+    if (this.current !== old) {
+        this.animate();
+    } else {
+        this.elements.previous.hide();
+    }
+};
+Slider.prototype.animate = function (animate) {
+    var args = {marginLeft: -this.positions[this.current]};
+    if (typeof animate !== undefined) {
+        this.elements.container.stop().animate(args, this.duration);
+    } else {
+        this.elements.container.css(args);
+    }
+};
